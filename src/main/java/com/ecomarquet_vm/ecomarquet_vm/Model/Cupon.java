0ac1167d000810ba712853de0155dd5d74e86a86
Sieglif13 <benjacaqui@gmail.com>
2025-05-28 23:40:43 -0400

    Arreglado

--- src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Cupon.java
+++ src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Cupon.java
@@ -1,5 +1,28 @@
 package com.ecomarquet_vm.ecomarquet_vm.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "cupones")
 public class Cupon {
 
+    @Id
+    @Column(strategy = GenerationType.IDENTITY)
+    private String codigo;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal descuento;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "valido_hasta", nullable = false)
+    private Date validoHasta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_transaccion", nullable = false)
+    private Transaccion transaccion;
 }
