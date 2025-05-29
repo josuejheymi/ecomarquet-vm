60a2d38666f274269e104f62301d263fada9275c
LeandroDuoc <lea.vergarar@duocuc.cl>
2025-05-29 06:25:58 -0400

    Por favor, subete

--- src/main/java/com/ecomarquet_vm/ecomarquet_vm/Controller/PedidoController.java
+++ src/main/java/com/ecomarquet_vm/ecomarquet_vm/Controller/PedidoController.java
@@ -1,5 +1,42 @@
 package com.ecomarquet_vm.ecomarquet_vm.Controller;
 
-public class PedidoController {
+import com.ecomarquet_vm.ecomarquet_vm.Model.Pedido;
+import com.ecomarquet_vm.ecomarquet_vm.Service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/facturas")
+public class PedidoController {
+    @Autowired
+    private PedidoService pedidoService;
+
+    @GetMapping
+    public List<Pedido> getAll(){
+        return pedidoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pedido getById(@PathVariable Long id) {
+        return pedidoService.findById(id);
+    }
+
+    @PostMapping
+    public Pedido create(@RequestBody Pedido pedido) {
+        return pedidoService.save(pedido);
+    }
+
+    @PutMapping("/{id}")
+    public Pedido save(@PathVariable Long id, @RequestBody Pedido pedido) {
+        pedido.setId(id);
+        return pedidoService.save(pedido);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePedido(@PathVariable Long id) {
+        pedidoService.delete(id);
+    }
 }
--- src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Factura.java
+++ src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Factura.java
@@ -1,5 +1,36 @@
 package com.ecomarquet_vm.ecomarquet_vm.Model;
 
-public class Factura {
+import java.sql.Date;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "factura")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_factura")
+    private Long Id;
+
+    @Column(nullable = false, name = "fechaFactura")
+    private Date fecha;
+
+    @Column(nullable = false, length = 100)
+    private String detalles; 
+
+    @Column(nullable = false, length = 100)
+    private double total;
+
+    //Relacion con Transaccion
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_transaccion", referencedColumnName = "id")
+    private Transaccion transaccion;
 }
+
