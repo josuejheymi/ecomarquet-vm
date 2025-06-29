d113438336e0f101c1c09e0be428929035c83a20
ryiwuu <ta.pizarrop@duocuc.cl>
2025-06-29 13:07:47 -0400

    getter and setter

--- src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Factura.java
+++ src/main/java/com/ecomarquet_vm/ecomarquet_vm/Model/Factura.java
@@ -28,4 +28,37 @@ public class Factura {
     @OneToOne
     @JoinColumn(name = "transaccion_id", nullable = false, unique = true)
     private Transaccion transaccion;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public String getDetalles() {
+        return detalles;
+    }
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+    public BigDecimal getTotal() {
+        return total;
+    }
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+    public Transaccion getTransaccion() {
+        return transaccion;
+    }
+    public void setTransaccion (Transaccion transaccion) {
+        this.transaccion = transaccion;
+    }
+
 }
